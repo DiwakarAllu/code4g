c7bb311719277f7a6a8c05c0aa7492f56e55a0cc
Allu Diwakar <101788832+DiwakarAllu@users.noreply.github.com>
2025-08-03 19:10:40 +0530

    Maximum-fruits-harvested-after-at-most-k-steps

---
+++ leetcode_daily/2025-08/2025-08-03.java
@@ -0,0 +1,6 @@
+"""
+Date: August 3, 2025
+Problem: maximum-fruits-harvested-after-at-most-k-steps (LeetCode #2106)
+Link: https://leetcode.com/problems/maximum-fruits-harvested-after-at-most-k-steps/description/
+"""
+  
