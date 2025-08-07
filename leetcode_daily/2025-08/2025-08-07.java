699b7360674c9c157bd36e6a4d1621eccdd6accd
Allu Diwakar <101788832+DiwakarAllu@users.noreply.github.com>
2025-08-07 09:56:58 +0530

    find-the-maximum-number-of-fruits-collected

---
+++ leetcode_daily/2025-08/2025-08-07.java
@@ -0,0 +1,6 @@
+"""
+Date: August 7, 2025
+Problem: find-the-maximum-number-of-fruits-collected (LeetCode #3363)
+Link: https://leetcode.com/problems/find-the-maximum-number-of-fruits-collected/
+"""
+  
