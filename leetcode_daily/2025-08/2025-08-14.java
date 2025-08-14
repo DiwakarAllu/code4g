0048975626aa89f92f97dc850222e43d6bb635a8
Allu Diwakar <101788832+DiwakarAllu@users.noreply.github.com>
2025-08-14 16:26:19 +0530

    largest-3-same-digit-number-in-string

--- leetcode_daily/2025-08/2025-08-14.java
+++ leetcode_daily/2025-08/2025-08-14.java
@@ -4,8 +4,24 @@ Problem: largest-3-same-digit-number-in-string (LeetCode #2264)
 Link: https://leetcode.com/problems/largest-3-same-digit-number-in-string/description/
 """
 class Solution {
+  
+    private static final List<String> sameDigitNumbers = List.of(
+        "999", "888", "777", "666", "555", "444", "333", "222", "111", "000"
+    );
+
     public String largestGoodInteger(String num) {
+        for (String good : sameDigitNumbers) {
+            if (num.contains(good)) {
+                return good;
+            }
+        }
+        return "";
+    }
+
+
+    public String largestGoodInteger2(String num) {
         int n=num.length();
+        int max=0;
         String ans="";
         for(int i=0;i<n-2;i++){
             if(num.charAt(i)==num.charAt(i+1) && num.charAt(i)==num.charAt(i+2)){
@@ -17,6 +33,7 @@ class Solution {
 
             }
         }
+      
        return ans;
     }
 }
