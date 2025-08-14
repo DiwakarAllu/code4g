057ebf0d9c65ec00b2866c7e48ebbcdad2c0e269
Allu Diwakar <101788832+DiwakarAllu@users.noreply.github.com>
2025-08-14 16:20:14 +0530

    largest-3-same-digit-number-in-string

---
+++ leetcode_daily/2025-08/2025-08-14.java
@@ -0,0 +1,22 @@
+"""
+Date: August 14, 2025
+Problem: largest-3-same-digit-number-in-string (LeetCode #2264)
+Link: https://leetcode.com/problems/largest-3-same-digit-number-in-string/description/
+"""
+class Solution {
+    public String largestGoodInteger(String num) {
+        int n=num.length();
+        String ans="";
+        for(int i=0;i<n-2;i++){
+            if(num.charAt(i)==num.charAt(i+1) && num.charAt(i)==num.charAt(i+2)){
+               
+                String temp=num.substring(i,i+3);
+                if(ans.compareTo(temp)<0){  // a.compareTo(b) < 0 => a is less than b
+                    ans=temp;
+                }
+
+            }
+        }
+       return ans;
+    }
+}
